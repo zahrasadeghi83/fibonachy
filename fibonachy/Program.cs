99863230722aa36802c9d45dc611c290d10d9c94
ZS <ZS@DESKTOP-7TC45F3>
2024-08-10 20:03:39 +0330

    Add project files.

---
+++ fibonachy/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace fibonachy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int c;
+            int a = 0;
+            int b = 1;
+            int n = 10;
+            for (int i = 1; i < n; i++)
+            {
+                Console.WriteLine(b);
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            
+        }
+    }
+}
